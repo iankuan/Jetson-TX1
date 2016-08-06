04e6ac8c5184bfd6a3b1e2062a91d168b3928483
c14006078 <c14006078@gmail.com>
2016-08-06 05:05:33 +0000

    FIX: opencv install script

--- script/install/opencv_2.4.12_install.sh
+++ script/install/opencv_2.4.12_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Pre-Install
-sudo pt-get install build-essential cmake git libgtk2.0-dev pkg-config\
+sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config\
   libavcodec-dev libavformat-dev libswscale-dev libtbb2 libtbb-dev\
   libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev\
   cmake-curses-gui g++ libavutil-dev libv4l-dev libeigen3-dev\
@@ -11,7 +11,7 @@ sudo apt-get update
 #opencv-2.4.12 install
 cd ~/Downloads
 wget https://github.com/Itseez/opencv/archive/2.4.12.zip
-unzip opencv-2.4.12.zip
+unzip 2.4.12.zip
 cp -af opencv-2.4.12 ~
 cd ~/opencv-2.4.12
 mkdir release
@@ -19,7 +19,7 @@ cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D INSTALL_C_EXAMPLE=ON \
       ..
-make
+make -j4
 make -j4 install
 
 #env config
